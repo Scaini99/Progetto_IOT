2eb917a83ac155e5efa1e0e5470df1fcee71e2c3
Scaini Luca <scainiluca1999@gmail.com>
2025-05-29 11:20:56 +0200

    cambiato postgres, funziona tutto

--- code/examples/postgresProva.py
+++ code/examples/postgresProva.py
@@ -3,13 +3,12 @@ import pandas
 
 
 #---------------------------collegamento al database con visualizzazione della tabella--------------------
-database = psycopg2.connect( host="localhost",
+database = psycopg2.connect( host="192.168.1.82",
                            port=5432,
                            database="pacchi",
-                           user="postgres",
-                           password="adminpsql")
-#db=pandas.read_sql("SELECT*FROM dati_consegna", database)
-#print(db)
+                           user="admin",
+                           password="psqladmin")
+
 #----------------------------------funziona---------------------------------------------------------------
 
 #-------------------------------caricamento dati csv sulla tabella----------------------------------------
@@ -17,13 +16,13 @@ selettore = database.cursor()
 
 with open(r"C:\Users\scain\OneDrive\Desktop\provaCaricamento.csv", "r", encoding="utf-8") as f: # Salta intestazione CSV (header)
         next(f)
-        selettore.copy_expert("COPY dati_consegna FROM STDIN WITH CSV", f)
+        selettore.copy_expert("COPY dati_spedizione FROM STDIN WITH CSV", f)
 
 database.commit()
 
 
 print("Dati caricati con successo!")
-db=pandas.read_sql("SELECT*FROM dati_consegna", database)
+db=pandas.read_sql("SELECT*FROM dati_spedizione", database)
 print(db)
 selettore.close()
 database.close()
